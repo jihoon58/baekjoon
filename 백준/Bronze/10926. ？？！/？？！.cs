a999371f7b9b62cd59568eca58a0d1e49e2ac92f
Jun404 <kdhkdh181@gmail.com>
2025-04-04 20:28:09 +0900

    [Bronze V] Title: ??!, Time: 48 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,11 @@
+namespace baekjun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string junha = Console.ReadLine();
+            Console.WriteLine($"{junha}??!");
+        }
+    }
+}
