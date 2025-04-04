f0518bbbb75755aef50754e1933fec8d7d2070d7
Jun404 <kdhkdh181@gmail.com>
2025-04-04 21:04:48 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5336 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,10 @@
+namespace baekjun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+        }
+    }
+}
