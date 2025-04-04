8c27f541d72101d28ca1f41e8a7024db015f8974
Jun404 <kdhkdh181@gmail.com>
2025-04-04 20:57:48 +0900

    [Bronze V] Title: 고양이, Time: 44 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,14 @@
+namespace baekjun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+
+        }
+    }
+}
