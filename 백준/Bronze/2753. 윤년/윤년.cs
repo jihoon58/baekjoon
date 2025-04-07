98798d24481edaef2740dee09035e4035c67354c
Jun404 <kdhkdh181@gmail.com>
2025-04-07 12:51:00 +0900

    [Bronze V] Title: 윤년, Time: 48 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cs
@@ -0,0 +1,18 @@
+namespace baekjuntest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
